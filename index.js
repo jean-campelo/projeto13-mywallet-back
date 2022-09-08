2242c14ea41b21d0455aa2caa6005524094ac19b
Jean Campelo <jean93campelo@gmail.com>
2022-09-08 16:48:28 -0300

    Conectando com porta 5000

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
 const server = express();
 server.use(cors());
 server.use(express.json());
 const port = 5000;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("MyWallet");
+});
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
