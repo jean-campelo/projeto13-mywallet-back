62422e19d99d2163bf7c0042cd618d36d7cd007d
Jean Campelo <jean93campelo@gmail.com>
2022-09-08 16:55:18 -0300

    Recebendo dados e validando com biblioteca joi

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import joi from "joi";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
@@ -15,5 +16,28 @@ mongoClient.connect().then(() => {
   db = mongoClient.db("MyWallet");
 });
 
+const newUserSchema = joi.object({
+  name: joi.string().required().empty(" "),
+  email: joi.string().required().email(),
+  password: joi.string().required().empty(" "),
+  confirmPassword: joi.ref("password"),
+});
+
+server.post("/sign-up", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const validationNewUser = newUserSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationNewUser.error) {
+    const errors = validationNewUser.error.details.map(
+      (detail) => detail.message
+    );
+    return res.send(errors).sendStatus(422);
+  }
+
+  res.sendStatus(201);
+});
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
